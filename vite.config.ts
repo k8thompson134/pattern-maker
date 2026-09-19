0ae07df35341730a2c010a3a31082baded299448
Kate Thompson <katethompson134@gmail.com>
2026-09-18 19:28:16 -0500

    Allow Tailscale hostname for dev server access from other devices

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['kates-mac-mini.tail778117.ts.net'],
+  },
 })
